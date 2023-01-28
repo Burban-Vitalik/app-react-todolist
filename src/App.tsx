c9607c69bcf494f7fdc7424a86c65bb9f66ee2ac
Vitalik Burban <burbanvitalik2002@gmail.com>
2023-01-28 02:32:35 +0200

    TodoAppBar has already got ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoAppBar from './components/TodoBar/TodoBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoAppBar />
     </div>
   );
 }
---
+++ src/components/TodoBar/TodoBar.tsx
@@ -0,0 +1,300 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import TaskIcon from '@mui/icons-material/Task';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import RunningWithErrorsIcon from '@mui/icons-material/RunningWithErrors';
+
+// Color for App Bar Background
+import { grey } from '@mui/material/colors';
+const myGreyColor = grey[900];
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+export default function TodoAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+
+        {/* Point is Done */}
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <TaskIcon />
+          </Badge>
+        </IconButton>
+        <p>Done</p>
+      </MenuItem>
+
+      {/* Point is Active */}
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={5} color="error">
+            <RunningWithErrorsIcon />
+          </Badge>
+        </IconButton>
+        <p>Active</p>
+      </MenuItem>
+
+
+      {/* Point is All Tasks */}
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <ListAltIcon />
+          </Badge>
+        </IconButton>
+        <p>All Tasks</p>
+      </MenuItem>
+
+      {/* Point is Important */}
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Important</p>
+      </MenuItem>
+
+      
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1,}}>
+      <AppBar position="static" style={{backgroundColor: myGreyColor }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            Todo List
+          </Typography>
+          
+          {/* Search Area */}
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Create new task"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            
+                {/* Todos is Done */}
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <p style={{fontSize:"16px", marginRight:'10px'}}>Done</p>
+                <Badge badgeContent={4} color="error">
+                    <TaskIcon />
+                </Badge>
+                </IconButton>
+
+                {/* Todos is Active */}
+                <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+                >
+                <p style={{fontSize:"16px", marginRight:'10px'}}>Active</p>
+                <Badge badgeContent={20} color="error">
+                    <RunningWithErrorsIcon />
+                </Badge>
+                </IconButton>
+
+                {/* Todos is All Tasks */}
+                <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+                >
+                <p style={{fontSize:"16px", marginRight:'10px'}}>All Tasks</p>
+                <Badge badgeContent={17} color="error">
+                    <ListAltIcon />
+                </Badge>
+                </IconButton>
+
+                {/* Todos is Important */}
+                <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+                >
+                <p style={{fontSize:"16px", marginRight:'10px'}}>Important</p>
+                <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                </Badge>
+                </IconButton>
+
+                {/* point for profile */}
+                <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+                >
+                <AccountCircle />
+                </IconButton>
+          </Box>
+          {/* Mobile */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
+}
