945a766a0ed9e1603339bc3534aea06af11dc655
Vitalik Burban <burbanvitalik2002@gmail.com>
2023-02-01 17:25:20 +0200

    register page logic

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,21 +1,69 @@
+import React, {useEffect ,useState } from 'react';
 import './Register.scss';
 
+type UserType = {
+    id: number | string
+    fullName: string
+    email: string
+    password: string | number
+}
 
-export function RegisterPage(){
+export function RegisterPage(){    
+    let users = localStorage.getItem('arrNewUsers') ? JSON.parse(localStorage.getItem('arrNewUsers') || '') : [];
+
+    const [arrNewUsers, setArrNewUsers] = useState(users);
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+
+
+    function onChangeFullName(e:any){
+        setFullName(e.target.value);
+    }
+
+    function onChangeEmail(e:any){
+        setEmail(e.target.value);
+    }
+
+    function onChangePassword(e:any){
+        setPassword(e.target.value);
+    }
+
+    function createNewUser(){
+        let newUser:UserType = {
+            id: Math.random(),
+            fullName: fullName,
+            email: email,
+            password: password,
+        }
+
+
+        users.push(newUser);
+        setArrNewUsers([...users]);
+
+        setFullName('')
+        setEmail('')
+        setPassword('')
+    }
+
+    useEffect(()=>{
+        localStorage.setItem('arrNewUsers', JSON.stringify(arrNewUsers))
+    }, [arrNewUsers])
+
     return (
         <div className="registerPage">
             <div className='register_form'>
                 <h2>Register Account Form</h2>
                 <hr />
 
-                <label htmlFor="fullName">Full Name<input type="text" placeholder='Your Name'/></label>            
-                <label htmlFor="yourEmail">Your Email<input type='email'placeholder='Your Email'/></label>            
-                <label htmlFor="password">Password<input type="password" placeholder='Your Email'/></label>
+                <label htmlFor="fullName">Full Name<input type="text" placeholder='Your Name' value={fullName} onChange={onChangeFullName}/></label>            
+                <label htmlFor="yourEmail">Your Email<input type='email'placeholder='Your Email' value={email} onChange={onChangeEmail}/></label>            
+                <label htmlFor="password">Password<input type="password" placeholder='Your Password'value={password} onChange={onChangePassword}/></label>
 
                 <p className='linkAuth'>Register / Login</p>
-                <button>Register</button>            
+                <button onClick={createNewUser}>Register</button>            
             </div>
-
         </div>
     )
 }
